39680365e29f5ea104d4ca7ede5a0823fb350617
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-06-09 12:55:29 +0900

    23.06.09_수업

--- ContainerStudy/src/edu/pnu/study/ContainerMain.java
+++ ContainerStudy/src/edu/pnu/study/ContainerMain.java
@@ -14,29 +14,30 @@ public class ContainerMain {
 	private ArrayList<String> list = new ArrayList<>(Arrays.asList("+", "-", "*", "/"));
 	private MyOperatorContainer moc = new MyOperatorContainer();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main Thread!!
 		ContainerMain cm = new ContainerMain();
 
-		// 일정 시간동안 사용되지 않고 있는 Operator 객체를 제거하는 Thread
-//		Thread th = new Thread(() -> {
-//			MyOperatorContainer moc = cm.getContainer();
-//
-//			// 메인 Thread가 종료하면 같이 종료되도록 종료 조건 변수(threadCheck)를 확인한다.
-//			while(cm.getThreadCheck()) {
-//				// 생존 기준 시간인 millisecond를 넘겨서 사용되지 않은 객체는 제거한다.
-//				moc.releaseOldObject(10000);
-//				
-//				try {
-//					Thread.sleep(1000);
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//				System.out.println("thread");
-//			}
-//			System.out.println("쓰레드를 종료합니다.");
-//		});
-//		// Thread 시작
-//		th.start();
+		// 일정 시간동안 사용되지 않고 있는 Operator 객체를 제거하는 자식 Thread -> 자식 Thread를 생성
+		Thread th = new Thread(() -> {
+			MyOperatorContainer moc = cm.getContainer();
+
+			// 메인 Thread가 종료하면 같이 종료되도록 종료 조건 변수(threadCheck)를 확인한다.
+			while(cm.getThreadCheck()) {
+				System.out.println("Container Size: " + moc.getOperatorSize());
+				// 생존 기준 시간인 millisecond를 넘겨서 사용되지 않은 객체는 제거한다.
+				moc.releaseOldObject(15000);
+				
+				try {
+					Thread.sleep(1000); //-> 1초에 한번 씩!
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("thread");
+			}
+			System.out.println("쓰레드를 종료합니다.");
+		});
+		// 자식 Thread 시작, Thread가 실행되는 시점!!
+		th.start();
 		
 		System.out.println("프로그램을 시작합니다.");
 		
